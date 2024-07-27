1a7c3b575290acdf83857b91503305d14eeb45e5
Amit Biswas <amit.142biswas@gmail.com>
2024-07-27 22:13:52 +0530

    notes

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from '@clerk/nextjs'
+import React from 'react'
+
+function Header() {
+    const {user} = useUser()
+
+  return (
+    <div>
+        {user && (
+            <h1>
+            {user?.firstName}
+            {`'s`} Space
+        </h1>
+        )}
+
+        <div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+
+            <SignedIn>
+              <UserButton />               
+            </SignedIn>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
